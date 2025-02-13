bdd2fb1eaab729a354c899ea37e88f3ee551a0d2
hexadecimaloctal <hexadecimaloctal@gmail.com>
2025-02-13 12:27:59 +0530

    initail commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+from telegram import Update
+from telegram.ext import Application , CommandHandler , MessageHandler , filters, CallbackContext
+import os
+
+TOKEN=os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def start(update:Update,context:CallbackContext):
+    await update.message.reply_text("Hello I new bot ")
+
+async def echo (update: Update, context:CallbackContext):
+    await update.message.reply_text(update.message.text+"by bot")
+
+def main():
+    app=Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start",start))
+    app.add_handler(MessageHandler(filters.Text & ~ filters.COMMAND, echo))
+    print("Bot is running ...............")
+
+    app.run_polling()
+
+if __name__=="__main__":
+    main()
+
